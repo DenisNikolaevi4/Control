4325eeda39a04f376ee4a0229e6f47362e09c4c2
SkyLearn <ba_de@mail.ru>
2023-02-16 20:07:45 +0300

    Добавил остальные массивы

--- Program.cs
+++ Program.cs
@@ -19,3 +19,41 @@ for (int i = 0; i < array1.Length; i++)
 }
 Console.WriteLine();
 Console.WriteLine();
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+Console.Write($"Заданный массив: ");
+for (int j = 0; j < array2.Length; j++)
+{
+    Console.Write($"[{array2[j]}] ");
+}
+Console.WriteLine();
+Console.Write($"Элементы массива, где <= 3 символов: ");
+for (int i = 0; i < array2.Length; i++)
+{
+    result = array2[i];
+    if (result.Length < 4)
+    {
+        Console.Write($"{result} ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine();
+string[] array3 = { "Russia", "Denmark", "Kazan" };
+Console.WriteLine();
+
+Console.Write($"Заданный массив: ");
+for (int j = 0; j < array3.Length; j++)
+{
+    Console.Write($"[{array3[j]}] ");
+}
+Console.WriteLine();
+Console.Write($"Элементы массива, где <= 3 символов: ");
+for (int i = 0; i < array3.Length; i++)
+{
+    result = array3[i];
+    if (result.Length < 4)
+    {
+        Console.Write($"->{result} ");
+    }
+ 
+}
+
